7a42eac093bc296a8fcafb053e8ba71aac0ac7ac
Simon <einzinger.simon@googlemail.com>
2016-04-20 23:46:10 +0200

    Logger Properties hinzufgefügt, Schreibt Changes in Log-File

--- MusicChanges - Logger/src/de/einzinger/simon/musicChangesLogger/FileReader.java
+++ MusicChanges - Logger/src/de/einzinger/simon/musicChangesLogger/FileReader.java
@@ -1,28 +1,54 @@
 package de.einzinger.simon.musicChangesLogger;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class FileReader {
+
+	private static final String DESTINATION_PATH = "H:/Testmusik/musikLog.log";
 
 	public void printFiles(File[] files) {
 		for (File file : files) {
 			if (file.isDirectory()) {
-//				System.out.println("Verzeichnisname: " + file.getName());
 				printFiles(file.listFiles());
-//				System.out.println("_______________________________________");
 			} else {
 				DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
 				String today = formatter.format(new Date(System.currentTimeMillis()));
 				String date = formatter.format(new Date(file.lastModified()));
-				String output = date + " | " + file.getParent() + " | " + file.getName();
+				// String output = date + " | " + file.getParent() + " | " +
+				// file.getName();
+				String output = date + " | " + file.getAbsolutePath();
 				if (date.equals(today)) {
 					System.out.println(output);
+					log(output);
 				}
-				
 			}
 		}
+	}
+
+	private void log(String output) {
+		Logger logger = Logger.getLogger(this.getClass().getName());
+		Handler handler = null;
+		try {
+			handler = new FileHandler(DESTINATION_PATH, true);
+			Formatter formatter = new SimpleFormatter();
+			handler.setFormatter(formatter);
+			logger.addHandler(handler);
+		} catch (SecurityException | IOException e) {
+
+		}
+
+		logger.setLevel(Level.INFO);
+		logger.info(output);
+		handler.close();
 	}
 }
--- MusicChanges - Logger/src/de/einzinger/simon/musicChangesLogger/Main.java
+++ MusicChanges - Logger/src/de/einzinger/simon/musicChangesLogger/Main.java
@@ -1,12 +1,20 @@
 package de.einzinger.simon.musicChangesLogger;
 
 import java.io.File;
+import java.util.logging.LogManager;
 
 public class Main {
 
 	private static final String PATH = "H:/Testmusik";
 
 	public static void main(String[] args) {
+		System.setProperty("java.util.logging.config.file", "logging.properties");
+		try {
+			LogManager.getLogManager().readConfiguration();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		File[] files = new File(PATH).listFiles();
 		FileReader fileReader = new FileReader();
 		fileReader.printFiles(files);
